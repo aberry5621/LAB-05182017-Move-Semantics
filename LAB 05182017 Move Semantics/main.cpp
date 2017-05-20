1e25577c50941da9533d6e9af018970124d15169
aberry5621 <aberry5621@gmail.com>
2017-05-20 08:10:01 -0700

    Initial Commit

---
+++ LAB 05182017 Move Semantics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LAB 05182017 Move Semantics
+//
+//  Created by ax on 5/20/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
